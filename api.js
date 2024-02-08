501b3d424775e9a7bfd072281010ff024c8b8cde
Archy_k <artbilov@gmail.com>
2024-02-08 11:57:07 -0800

    fix search with symbols

--- api.js
+++ api.js
@@ -34,7 +34,7 @@ async function handleApi(req, res) {
 
     } else if (endpoint == 'search') {
       const { query, min, max, ...props } = params
-      const $regex = new RegExp(query)
+      const $regex = new RegExp(query.replace(/([^a-zA-Z0-9])/g, "\\$1"))
       let filter = query ?
         // { $or: [
         { name: { $regex, $options: "i" } }

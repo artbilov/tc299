e8fd12d460fe5d22bfa5059ad738f873559d7d31
Archy_k <artbilov@gmail.com>
2024-02-27 15:09:53 -0800

    try 100500

--- app.js
+++
@@ -1,7 +0,0 @@
-fetch('https://tc299.onrender.com/user', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({login: "artbilov", password: "123123aaa"})
-})

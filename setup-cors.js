3496270611ce22cd7fce7a52919e851f8b8941ea
artbilov <artbilov@gmail.com>
2024-05-07 12:36:20 -0700

    fix cors

--- setup-cors.js
+++ setup-cors.js
@@ -1,7 +1,19 @@
-function setupCORS(res) {
+function setupCORS(req, res) {
+
+  if (req.method === 'OPTIONS') {
+    // Установка заголовков CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
-  res.setHeader('Access-Control-Request-Method', '*')
+  // res.setHeader('Access-Control-Request-Method', '*')
+  res.setHeader('Access-Control-Request-Method', 'POST, GET, DELETE, PUT, OPTIONS')
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'POST, GET, DELETE, PUT, OPTIONS, key, Content-Type')
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')

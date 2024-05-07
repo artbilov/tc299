055b02497585a903c00340e963499859cf265f65
artbilov <artbilov@gmail.com>
2024-05-07 13:05:39 -0700

    fix cors test 2

--- setup-cors.js
+++ setup-cors.js
@@ -1,22 +1,24 @@
 function setupCORS(req, res) {
+  
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.google.com');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
 
   if (req.method === 'OPTIONS') {
     // Установка заголовков CORS
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.google.com');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // res.setHeader('Access-Control-Allow-Methods', '*');
     res.writeHead(200);
     res.end();
     return;
-  }
+  }  
 
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  // res.setHeader('Content-Type', 'application/json; charset=utf-8')
 
-  res.setHeader('Access-Control-Request-Method', 'POST, GET, DELETE, PUT, OPTIONS')
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'POST, GET, DELETE, PUT, OPTIONS, key, Content-Type')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')
+  // res.setHeader('Access-Control-Request-Method', 'POST, GET, DELETE, PUT, OPTIONS')
+  // res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Headers', 'POST, GET, DELETE, PUT, OPTIONS, key, Content-Type')
  
 }
 

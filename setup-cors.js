8179807ccd57a9d56cbc1880dd33a599510f4a47
artbilov <artbilov@gmail.com>
2024-05-07 22:57:22 -0700

    fix cors test 5

--- setup-cors.js
+++ setup-cors.js
@@ -1,18 +1,17 @@
 function setupCORS(req, res) {
   
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', 'origin, Content-Type, accept');
-  // res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
-
-
+  
   if (req.method === 'OPTIONS') {
     // Установка заголовков CORS
-    // res.setHeader('Access-Control-Allow-Methods', '*');
     res.writeHead(200);
     res.end();
-    // return;
+    return;
   }  
-
+  
+  // res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   // res.setHeader('Content-Type', 'application/json; charset=utf-8')
 

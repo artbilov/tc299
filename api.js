c81e853030c1e47d9888666b15d3243ef95b6b4c
artbilov <artbilov@gmail.com>
2024-05-08 01:40:32 -0700

    fix cors test 17

--- api.js
+++ api.js
@@ -20,7 +20,7 @@ function makeApiHandler(db) {
     // db.collection('users').createIndex({ email: 1 }, { unique: true })
     // db.collection('users').createIndex({ login: 1 }, { unique: true })
 
-    // setupCORS(req, res)
+    setupCORS(req, res)
 
     await ensureSession(req, res)
 

e1ecd869143c62b45d3d2530388623d637e71e97
artbilov <artbilov@gmail.com>
2024-05-07 22:49:09 -0700

    cors fix test 3

--- setup-cors.js
+++ setup-cors.js
@@ -1,16 +1,16 @@
 function setupCORS(req, res) {
   
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.google.com');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'origin, Content-Type, accept');
+  // res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
 
 
-  if (req.method === 'OPTIONS') {
+  // if (req.method === 'OPTIONS') {
     // Установка заголовков CORS
     // res.setHeader('Access-Control-Allow-Methods', '*');
-    res.writeHead(200);
-    res.end();
-    return;
+    // res.writeHead(200);
+    // res.end();
+    // return;
   }  
 
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
@@ -20,7 +20,7 @@ function setupCORS(req, res) {
   // res.setHeader('Access-Control-Allow-Origin', '*')
   // res.setHeader('Access-Control-Allow-Headers', 'POST, GET, DELETE, PUT, OPTIONS, key, Content-Type')
  
-}
+// }
 
 exports.setupCORS = setupCORS;
 

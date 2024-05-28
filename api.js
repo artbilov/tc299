acaad62954b68ba08242ab22b8fc0541360dd161
artbilov <artbilov@gmail.com>
2024-05-28 12:26:53 -0700

    fix cors test 66

--- api.js
+++ api.js
@@ -27,7 +27,7 @@ function makeApiHandler(db) {
 
     setupCORS(req, res, origin)
 
-    // console.log("Request: " + req)
+    console.log("Request: " + req)
 
     if (method === 'OPTIONS') return
 

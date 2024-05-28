bfe904de380da6f3e22c45656e56a020c059ea0c
artbilov <artbilov@gmail.com>
2024-05-28 12:47:15 -0700

    fix cors test 69

--- gen-cookie.js
+++ gen-cookie.js
@@ -4,6 +4,6 @@ function genCookie(name, value, days = 0, hours = 0, minutes = 0) {
   if (!days && !hours && !minutes) days = 1
   const milliseconds = ((days * 24 + hours) * 60 + minutes) * 60 * 1000
   const expire = new Date(Date.now() + milliseconds)
-  const cookie = name + '=' + value + '; secure; path=/; expires=' + expire.toUTCString()
+  const cookie = name + '=' + value + '; SameSite=None; Secure; path=/; expires=' + expire.toUTCString()
   return cookie
 }

2c73893eb2c26d8ad3516c2b62aa71132f0af097
artbilov <artbilov@gmail.com>
2024-05-30 12:50:22 -0700

    fix cors test 89

--- setup-cors.js
+++ setup-cors.js
@@ -1,19 +1,16 @@
 function setupCORS(req, res, origin) {
   // Установка заголовков CORS
-
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD');
 
     // for Cookies
     res.setHeader('Access-Control-Allow-Credentials', 'true')
-    // res.setHeader('Access-Control-Allow-Headers', 'Cookie, Content-Type')
     res.setHeader('Access-Control-Allow-Headers',  'Content-Type, Cookie, credentials, user-agent')
     res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie')
     res.setHeader('Content-Type', 'application/json')
 
     if (req.method === 'OPTIONS') {
       res.statusCode = 200
-      // res.setHeader('Access-Control-Allow-Credentials', 'true')
       res.end('Preflight request has succeeded.')
     }
   }
@@ -21,10 +18,3 @@ function setupCORS(req, res, origin) {
 
   module.exports = { setupCORS }
 
-
-// res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT')
-// res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-// res.setHeader('Content-Type', 'application/json; charset=utf-8')
-
-// res.setHeader('Access-Control-Allow-Origin', '*')
-// res.setHeader('Access-Control-Allow-Headers', 'POST, GET, DELETE, PUT, OPTIONS, key, Content-Type')
